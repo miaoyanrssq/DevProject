387e553498a29d161040a05d3be4642f8475a11e
高阳· 甄 <627312987@qq.com>
2018-09-13 16:06:40 +0800

    添加状态栏工具类

--- user/src/main/java/cn/zgy/user/UserActivity.java
+++ user/src/main/java/cn/zgy/user/UserActivity.java
@@ -2,12 +2,39 @@ package cn.zgy.user;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class UserActivity extends AppCompatActivity {
+import cn.zgy.base.BaseActivity;
+import cn.zgy.base.StatusBarUtil;
+
+public class UserActivity extends BaseActivity {
+
+    Toolbar toolbar;
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_user;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
+
+        initView();
+
+        //状态栏透明和间距处理
+        StatusBarUtil.immersive(this);
+        StatusBarUtil.setPaddingSmart(this, toolbar);
+        StatusBarUtil.setPaddingSmart(this, findViewById(R.id.blurView));
+    }
+
+    private void initView() {
+        toolbar = findById(R.id.toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
