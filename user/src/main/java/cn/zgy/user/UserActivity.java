60e36cb0d9c2a84b73f5822fe5d2fafb33268a48
高阳· 甄 <627312987@qq.com>
2018-09-21 15:49:15 +0800

    switchView， CicleImageView

--- user/src/main/java/cn/zgy/user/UserActivity.java
+++ user/src/main/java/cn/zgy/user/UserActivity.java
@@ -4,14 +4,20 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import cn.zgy.autoview.SwitchView;
+import cn.zgy.autoview.circleView.CircleImageView;
 import cn.zgy.base.BaseActivity;
 import cn.zgy.base.utils.StatusBarUtil;
 
-public class UserActivity extends BaseActivity {
+public class UserActivity extends BaseActivity implements SwitchView.SlideListener{
 
-    ImageView icon;
+    CircleImageView icon;
     Toolbar toolbar;
+
+    SwitchView switchView1, switchView2;
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_user;
@@ -48,5 +54,21 @@ public class UserActivity extends BaseActivity {
                 toPath("/MediaSelectActivity", 0, bundle);
             }
         });
+
+        switchView1 = findById(R.id.switch1);
+        switchView1.setSlideListener(this);
+        switchView2 = findById(R.id.switch2);
+    }
+
+    @Override
+    public void open() {
+        Toast.makeText(this, "tab2可以切换", Toast.LENGTH_SHORT).show();
+        switchView2.setSlideable(true);
+    }
+
+    @Override
+    public void close() {
+        switchView2.setSlideable(false);
+        Toast.makeText(this, "tab2禁止切换", Toast.LENGTH_SHORT).show();
     }
 }
